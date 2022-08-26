18df2f640ca293dc23b574ac256b11827d085fb9
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-08-25 22:16:34 -0400

    commit tree 597944bfbff502d9f8c72045bb609031519e8028

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+// declare a function taking in an array and a string
+// function findMatching(array, string) {
+//     // return the value of the filter method with => func to match element to string lowercased
+//     return array.filter(ele => ele.toLowerCase() === string.toLowerCase())
+// }
+
+// // declare function taking array and string
+// function fuzzyMatch(array, string) {
+//     // return filter method => function matching arr ele with string with charAt method
+//     return array.filter(ele => ele.charAt() === string.charAt())
+// }
+
+// // declare function with driver array of obj and string
+// function matchName(driver, string) {
+//     //return filter method => with ele in object with property 'name' and match with string
+//     return driver.filter(ele => ele.name == string)
+// }
+
+function findMatching(array, string) {
+   return array.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(name => name.toLowerCase().charAt(0) === string.toLowerCase().charAt(0))
+}
+
+function matchName(driveObj, string){
+    return driveObj.filter(id => id.name === string)
+}
